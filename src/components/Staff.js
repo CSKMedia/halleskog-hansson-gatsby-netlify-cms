8e6e59baa4267e406c36d1f8401ae57701bb0a41
Casper Karlsson <casper.karlsson@billhop.se>
2021-03-17 17:11:36 +0100

    Separeated teams to solna and vallentuna. Updated content

--- src/components/Staff.js
+++ src/components/Staff.js
@@ -32,7 +32,7 @@ export default () => (
       <section className="section">
         <div className="container">
           <div className="content has-text-centered">
-            <h1>Team Vallentuna</h1>
+            <h1 style={{ padding: "10px 0px"}}>Team Vallentuna</h1>
             {data && data.allDataYaml.edges.map(({node}) => (
               <div className="columns is-multiline">
                 {node.teamVallentuna && node.teamVallentuna.map(member => (
@@ -40,14 +40,14 @@ export default () => (
                     <div className="card">
                       <div className="card-image">
                         <figure class="image">
-                          <img className="is-rounded" style={{ border: "7px solid #f8f9fa"}} src={member.photo}/>
+                          <img className="is-rounded" style={{ border: "7px solid #f8f9fa", maxWidth: 250}} src={member.photo}/>
                         </figure>
                       </div>
                       <div class="card-content is-family-secondary">
-                      <div className="is-size-4 has-text-weight-bold is-family-primary">{member.name}</div>
-                      <div className="is-size-5 has-text-grey has-text-weight-medium">{member.position}</div>
-                      <div className="is-size-5 has-text-grey has-text-weight-medium">{member.phone}</div>
-                      <div className="is-size-5 has-text-weight-medium">
+                      <div className="is-size-5 has-text-weight-bold is-family-primary">{member.name}</div>
+                      <div className="is-size-6 has-text-grey has-text-weight-medium">{member.position}</div>
+                      <div className="is-size-6 has-text-grey has-text-weight-medium">{member.phone}</div>
+                      <div className="is-size-6 has-text-weight-medium">
                         <a href={`mailto:${member.email}`}>{member.email}</a>
                       </div>
                       </div>
@@ -57,8 +57,8 @@ export default () => (
               </div>
             ))}
           </div>
-          <div className="content has-text-centered">
-            <h1>Team Solna</h1>
+          <div className="content has-text-centered" style={{ marginTop: 20}}>
+            <h1 style={{ padding: "10px 0px"}}>Team Solna</h1>
             {data && data.allDataYaml.edges.map(({node}) => (
               <div className="columns is-multiline">
                 {node.teamSolna && node.teamSolna.map(member => (
@@ -66,14 +66,14 @@ export default () => (
                     <div className="card">
                       <div className="card-image">
                         <figure class="image">
-                          <img className="is-rounded" style={{ border: "7px solid #f8f9fa"}} src={member.photo}/>
+                          <img className="is-rounded" style={{ border: "7px solid #f8f9fa", maxWidth: 250}} src={member.photo}/>
                         </figure>
                       </div>
                       <div class="card-content is-family-secondary">
-                      <div className="is-size-4 has-text-weight-bold is-family-primary">{member.name}</div>
-                      <div className="is-size-5 has-text-grey has-text-weight-medium">{member.position}</div>
-                      <div className="is-size-5 has-text-grey has-text-weight-medium">{member.phone}</div>
-                      <div className="is-size-5 has-text-weight-medium">
+                      <div className="is-size-5 has-text-weight-bold is-family-primary">{member.name}</div>
+                      <div className="is-size-6 has-text-grey has-text-weight-medium">{member.position}</div>
+                      <div className="is-size-6 has-text-grey has-text-weight-medium">{member.phone}</div>
+                      <div className="is-size-6 has-text-weight-medium">
                         <a href={`mailto:${member.email}`}>{member.email}</a>
                       </div>
                       </div>
