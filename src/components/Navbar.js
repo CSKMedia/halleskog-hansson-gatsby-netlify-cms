f431ee6dccc42f2555a8e5814ed6fec6461d07ee
Casper Karlsson <casper.karlsson@billhop.se>
2021-03-24 08:51:31 +0100

    navbar phone error 2

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -141,11 +141,14 @@ export default function Navbar () {
             </div>
           </div>
           <div className="navbar-end">
+            { data.dataYaml ? (
             <div className="is-hidden-mobile navbar-item" style={{ display: "flex", fontSize: "0.7rem", color: "white", alignItems: "center"}}>
               <div style={{marginRight: "2rem"}}>kontor: {data.dataYaml.phone || ""} </div>
               <div style={{}}>Depå: {data.dataYaml.phone2 || ""}</div>
             {/* <FontAwesomeIcon icon={faMobileAlt} size="2x" color="#b60f1d" style={{marginRight: 5, color: "white"}}/> {data.dataYaml.phone} */}
             </div>
+            ) : (<></>)
+            }
           </div>
         </div>
       </div>
