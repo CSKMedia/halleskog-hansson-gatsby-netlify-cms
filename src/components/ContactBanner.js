d8c1caf707dac8af668e7ac773cd2802e196e6d0
Casper Karlsson <casper.karlsson@billhop.se>
2021-03-26 17:25:05 +0100

    fixed arrow color in navbar

--- src/components/ContactBanner.js
+++ src/components/ContactBanner.js
@@ -6,9 +6,8 @@ const ContactBanner = ({backgroundColor}) => {
 
   console.log("backgroundColor", backgroundColor)
 
-
   return (
-    <div className="is-flex is-justify-content-center" style={{ width: "100%", backgroundColor: `${backgroundColor || "green"}`, padding: "4rem"}}>
+    <div style={{ textAlign: "center", width: "100%", backgroundColor: `${backgroundColor || "green"}`, padding: "4rem"}}>
       <span style={{fontSize: "2rem", fontWeight:"bold", paddingRight: "10px"}}>Tveka inte att </span>
       <Link className="btn" to="/kontakt" style={{textAlign: "center"}}>
         Kontakta oss
