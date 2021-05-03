1c79d635ab9e75d0fb970f6a510b6fd828d32f53
Casper Karlsson <casper.karlsson@billhop.se>
2021-05-03 14:02:55 +0200

    fixed lowercase on nyheter

--- src/pages/nyheter/index.js
+++ src/pages/nyheter/index.js
@@ -20,7 +20,7 @@ export default class BlogIndexPage extends React.Component {
             style={{
               // boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #f40',
               // backgroundColor: '#f40',
-              textTransform: 'uppercase',
+              // textTransform: 'uppercase',
               color: 'white',
               padding: '1rem',
             }}
